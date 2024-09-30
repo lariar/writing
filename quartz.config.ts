adbe735796ea5b5f741e0300c287b9ce197f6ed8
lariar <randy.lariar@gmail.com>
2024-09-29 22:42:21 -0400

    🛠️ quartz.config.ts -> updated pageTitle to reflect change

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Randy Lariar - Blog",
+    pageTitle: "Randy Lariar's Writing",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
