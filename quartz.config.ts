ae4c8e58486cd8bd2137260d266d14e39b023d50
lariar <randy.lariar@gmail.com>
2024-09-29 21:44:03 -0400

    🛠️ quartz.config.ts -> Updated baseUrl to GitHub Pages.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "blog.lariar.com",
+    baseUrl: "lariar.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
